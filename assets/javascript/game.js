2da74b0731be9c2c2c86d478e17b85ed66ec4633
Brady Douthit <brady.douthit99@gmail.com>
2019-06-03 18:15:00 -0400

    fixed a bug with usedLetters.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,10 @@ var usedLetters = Array();
 var guessesLeft = 10;
 var possibleAnswers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var rightAnswer = possibleAnswers[Math.floor(Math.random()*possibleAnswers.length)];
+var newAnswer = possibleAnswers[Math.floor(Math.random()*possibleAnswers.length)];
 var guessesSoFar = document.getElementById("guesses-so-far");
 var wins = 0;
 var losses = 0;
-
-document.getElementById("right-answer").innerHTML = rightAnswer;
 
 document.onkeyup = function (event) {
     var guess = (event.key)
@@ -17,10 +16,12 @@ document.onkeyup = function (event) {
     if (rightAnswer.includes(guess)) {
         wins++;
         guessesLeft = 10;
+        usedLetters = Array();
     }
     if (! rightAnswer.includes(guess) && guessesLeft <= 0) {
         losses++
         guessesLeft = 10;
+        usedLetters = Array();
     }
     
     guessesSoFar.textContent = usedLetters;
@@ -28,4 +29,6 @@ document.onkeyup = function (event) {
     document.getElementById("wins").innerHTML = wins; 
     document.getElementById("losses").innerHTML = losses;
     document.getElementById("right-answer").innerHTML = rightAnswer;
+
 };
+
