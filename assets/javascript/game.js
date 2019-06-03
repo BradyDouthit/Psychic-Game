0d2266c9e37a2a109c84f71c9341587b48dbb335
Brady Douthit <brady.douthit99@gmail.com>
2019-06-03 16:41:51 -0400

    fixed things: win counter, loss counter, guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,31 @@
 var usedLetters = Array();
-//records keys pressed
+var guessesLeft = 10;
+var possibleAnswers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var rightAnswer = possibleAnswers[Math.floor(Math.random()*possibleAnswers.length)];
 var guessesSoFar = document.getElementById("guesses-so-far");
+var wins = 0;
+var losses = 0;
+
+document.getElementById("right-answer").innerHTML = rightAnswer;
+
 document.onkeyup = function (event) {
-    if (! usedLetters.includes(event.key)){
-        usedLetters.push(event.key)
+    var guess = (event.key)
+    if (! usedLetters.includes(guess)){
+        usedLetters.push(guess)
+        guessesLeft--
     }
+    if (rightAnswer.includes(guess)) {
+        wins++;
+        guessesLeft = 10;
+    }
+    if (! rightAnswer.includes(guess) && guessesLeft <= 0) {
+        losses++
+        guessesLeft = 10;
+    }
+    
     guessesSoFar.textContent = usedLetters;
-};
-//counts down guesses left when a key is pressed
-var i = document.getElementById("guesses-left");
-document.onkeydown = function (event) {
-    for (i = 10; i > 9; i--){
-    if (i < 1) {
-        document.write("you lose");
-    }
-}
+    document.getElementById("guesses-left").innerHTML = " " + guessesLeft;  
+    document.getElementById("wins").innerHTML = wins; 
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("right-answer").innerHTML = rightAnswer;
 };
